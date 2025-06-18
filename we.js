11895a92bf1db3f2b96a5e013d921269f5b17abb
Maverickcodes-sketch <mishrashaswat29@gmail.com>
2025-06-18 23:32:57 +0530

    Update we.js

--- we.js
+++ we.js
@@ -1,6 +1,6 @@
 document.getElementById('searchButton').addEventListener('click', function() {
     const location = document.getElementById('locationInput').value;
-    const apiKey = 'b7eba44dd0bd8125f24147605a5e7ea2'; // Replace with your OpenWeatherMap API key
+    const apiKey = 'b7eba44dd0bd8125f24147605a5e7ea2'; 
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
 
     fetch(apiUrl)
